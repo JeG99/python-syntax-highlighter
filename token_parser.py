aaebd00ffbd40820d3ce0eb2db8c343cedd6da8e
einar <einarlop0@gmail.com>
2021-09-02 12:33:49 -0500

    Parser kinda works now except for parenthesis in lists

---
+++ token_parser.py
@@ -0,0 +1,64 @@
+# <prog> ::= <exp> <prog> | $
+# <exp> ::= <átomo> | <lista>
+# <átomo> ::= símbolo | <constante>
+# <constante> ::= número | booleano | string
+# <lista> ::= ( <elementos> )
+# <elementos> ::= <exp> <elementos> | 
+
+# <prog> ::= <exp> <prog> | $
+# <exp> ::= simbolo | número | booleano | string | <lista>
+# <lista> ::=
+#
+#
+
+import sys
+import return_token as scanner
+
+
+def match(expectedToken):
+    global token
+    if token == expectedToken:
+        token = scanner.get_token()
+    else:
+        error("Wrong token")
+
+
+def parser():
+    global token
+    token = scanner.get_token()
+    prog()
+    if token == scanner.END:
+        print("Expresion bien construida")
+    else:
+        error("Expresion mal terminada")
+
+
+def prog():
+    exp()
+
+
+def exp():
+    if token == scanner.NUM:
+        match(token)
+        exp()
+    elif token == scanner.STR:
+        match(token)
+        exp()
+    elif token == scanner.BOO:
+        match(token)
+        exp()
+    elif token == scanner.SYM:
+        match(token)
+        exp()
+    elif token == scanner.LRP:
+        match(token)
+        exp()
+        match(scanner.RRP)
+        exp()
+    else:
+        error("Expresion mal iniciada")
+
+
+def error(msg):
+    print("ERROR:", msg)
+    sys.exit(1)
