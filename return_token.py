c4b3f8f63bfb652072fed9cc99b0ef23ff80b853
JeG99 <a00824494@itesm.mx>
2021-09-06 17:28:08 -0500

    cleanup and add styled html generation

--- return_token.py
+++ return_token.py
@@ -8,21 +8,17 @@ LRP = 501  # LEFT PARENTHESIS
 RRP = 502  # RIGHT PARENTHESIS
 END = 600  # END
 ERR = 700  # ERROR
-#      a-z 0-9   #   " t/f spc  \n   (    )   rar   $
-MT = [[1,  2,  3,  5,  1,  0,   0,  LRP, RRP,  7, END],  # 0 -> Intial State
-      [1,  7,  7,  7,  1, SYM, SYM, SYM, SYM,  7,  7],  # 1 -> Symbol
-      [7,  2,  7,  7,  7, NUM, NUM, NUM, NUM,  7,  7],  # 2 -> Number
-      [7,  7,  7,  7,  4, ERR,  7,   7,   7,  7,  7],  # 3 -> # of Bool
-      [7,  7,  7,  7,  7, BOO, BOO, BOO, BOO,  7,  7],  # 4 -> t/f of Bool
-      [5,  5,  7,  6,  5,  5,   7,   7,   7,  7,  7],  # 5 -> String Char
-      # 6 -> Closing quotes Strings
-      [7,  7,  7,  7,  7, STR, STR, STR, STR,  7,  7],
-      [7,  7,  7,  7,  7, ERR,  7,   7,   7,  7, ERR]]  # 7 -> Error state
+#      a-z 0-9   #   " t/f spc  \n   (   ) rar   $
+MT = [[  1,  2,  3,  5,  1,  0,  0,LRP,RRP,  7,END],  # 0 -> Intial State
+      [  1,  7,  7,  7,  1,SYM,SYM,SYM,SYM,  7,  7],  # 1 -> Symbol
+      [  7,  2,  7,  7,  7,NUM,NUM,NUM,NUM,  7,  7],  # 2 -> Number
+      [  7,  7,  7,  7,  4,ERR,  7,  7,  7,  7,  7],  # 3 -> # of Bool
+      [  7,  7,  7,  7,  7,BOO,BOO,BOO,BOO,  7,  7],  # 4 -> t/f of Bool
+      [  5,  5,  7,  6,  5,  5,  7,  7,  7,  7,  7],  # 5 -> String Char
+      [  7,  7,  7,  7,  7,STR,STR,STR,STR,  7,  7],  # 6 -> Closing quotes Strings
+      [  7,  7,  7,  7,  7,ERR,  7,  7,  7,  7,ERR]]  # 7 -> Error state
 
-# Porque se loopea estado de error? Error solo muere hasta espacio
-
-
-def filter(c):
+def filter(c, file):
     if c == 't' or c == 'f':  # true of false
         return 4
     elif c == '0' or c == '1' or c == '2' or \
@@ -36,8 +32,10 @@ def filter(c):
     elif ord(c) >= ord('a') and ord(c) <= ord('z'):  # a-z
         return 0
     elif c == ' ':  # blank space
+        file.write(c)
         return 5
     elif c == '\n':  # new line
+        file.write('<br>')
         return 6
     elif c == '(':  # delimitator (
         return 7
@@ -54,6 +52,7 @@ _read = True
 
 
 def get_token():
+    html = open('index.html', 'a')
     global _c, _read
     state = 0
     lexeme = ""
@@ -64,37 +63,45 @@ def get_token():
                 _c = sys.stdin.read(1)
             else:
                 _read = True
-            state = MT[state][filter(_c)]
+            state = MT[state][filter(_c, html)]
             if state < 100 and state != 0:
                 lexeme += _c
         if state == SYM:
             _read = False
             print("Symbol", lexeme)
+            html.write('<sym>' + str(lexeme) + '</sym>')
             return SYM
         elif state == NUM:
             _read = False
             print("Number", lexeme)
+            html.write('<num>' + str(lexeme) + '</num>')
             return NUM
         elif state == BOO:
             _read = False
             print("Boolean", lexeme)
+            html.write('<boo>' + str(lexeme) + '</boo>')
             return BOO
         elif state == STR:
             _read = False
             print("String", lexeme)
+            html.write('<str>' + str(lexeme) + '</str>')
             return STR
         elif state == LRP:
             lexeme += _c
             print("Delimitator", lexeme)
+            html.write('<lrp>' + str(lexeme) + '</lrp>')
             return LRP
         elif state == RRP:
             lexeme += _c
             print("Delimitator", lexeme)
+            html.write('<rrp>' + str(lexeme) + '</rrp>')
             return RRP
         elif state == END:
             print("Fin de expresion")
+            html.write('<end>' + '$' + '</end>')
             return END
         else:
             _read = False
             print("Error, not accepted word", lexeme)
             return ERR
+        html.close()
--- token_parser.py
+++ token_parser.py
@@ -30,12 +30,16 @@ def match(expectedToken):
     else:
         error("Wrong token")
 
-
 def parser():
     global token
+    html = open('index.html', 'w')
+    html.write('<!DOCTYPE html>\n<html>\n<head>\n<meta charset="utf-8">\n<link href="style.css" rel="stylesheet" type="text/css">\n</head>\n<body>\n')
+    html.close()
     token = scanner.get_token()
     prog()
-
+    html = open('index.html', 'a')
+    html.write('</body>\n</html>')
+    html.close()
 
 def prog():
     if token == scanner.SYM or token == scanner.NUM or token == scanner.STR or token == scanner.BOO or token == scanner.LRP:
@@ -47,7 +51,6 @@ def prog():
         else:
             error("Expresion mal terminada")
 
-
 def exp():
     if token == scanner.LRP:
         print("Inside exp", token)
@@ -55,13 +58,11 @@ def exp():
     else:
         atm()
 
-
 def atm():
     if token == scanner.SYM:
         match(token)
     else:
         con()
-
 
 def con():
     if token == scanner.NUM:
@@ -73,19 +74,18 @@ def con():
     else:
         error("Se esperaba una constante")
 
-
 def lis():
     match(scanner.LRP)  # reconoce Delimitador (
     ele()
     match(scanner.RRP)
-
 
 def ele():
     if token == scanner.SYM or token == scanner.NUM or token == scanner.STR or token == scanner.BOO or token == scanner.LRP:
         exp()
         ele()
 
-
 def error(msg):
-    print("ERROR:", msg)
+    html = open('index.html', 'w')
+    html.write('<!DOCTYPE html>\n<html>\n<head>\n<meta charset="utf-8">\n<link href="style.css" rel="stylesheet" type="text/css">\n</head>\n<body>\n<err>ERROR: ' + msg + '</err>\n</body>\n<html>') 
+    html.close()
     sys.exit(1)
